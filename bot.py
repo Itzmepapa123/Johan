96a7a321fb067c4292f630cfdead0a3ddce48b84
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-20 13:05:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT
-abab = -1002225858852
+abab = "-1002225858852"
 
 name ="""
 ░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗  ██╗░░██╗██╗░░░██╗███████╗
